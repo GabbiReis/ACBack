9586632b4c4bc7358c74db124c8476f7c4474d8b
Gabriela Reis <100102288+GabbiReis@users.noreply.github.com>
2024-06-09 18:41:41 -0300

    Adicionando novas funcionalidades

--- AppMobile/Program.cs
+++ AppMobile/Program.cs
@@ -1,53 +1,107 @@
 using AppMobile.AppMobile.Dados;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddDbContext<AppMobileContext>(options =>
-    options.UseSqlServer(@"Data Source=201.62.57.93,1434;
-                            User ID=RA046051;
-                            Password=046051;
-                            TrustServerCertificate=true"));
+// Adicione serviços ao contêiner.
+builder.Services.AddScoped<JwtService>();
 
-//builder.Services.AddDbContext<AppMobileContext>(options =>
-//    options.UseSqlServer(@"Data Source=10.107.176.41,1434;
-//                            User ID=RA046051;
-//                            Password=046051;
-//                            TrustServerCertificate=true"));
+// Adicione CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod());
+});
 
-
-
-
-
-// Adicione o serviço do Swagger
+// Adicione Swagger
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "AppMobile", Version = "v1" });
+
+    // Adicione a definição de segurança para o JWT
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+          Enter 'Bearer' [space] and then your token in the text input below.
+          \r\n\r\nExample: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+          new OpenApiSecurityScheme
+          {
+            Reference = new OpenApiReference
+            {
+              Type = ReferenceType.SecurityScheme,
+              Id = "Bearer"
+            },
+            Scheme = "oauth2",
+            Name = "Bearer",
+            In = ParameterLocation.Header,
+
+          },
+          new List<string>()
+        }
+    });
 });
+
+// Configure autenticação JWT
+var key = Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Key"]);
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"]
+    };
+});
+
+
+builder.Services.AddDbContext<AppMobileContext>(options =>
+    options.UseSqlServer(@"Data Source=201.62.57.93,1434;User ID=RA046051;Password=046051;database=BD046051;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False"));
+    //options.UseSqlServer(@"Data Source=10.107.176.41,1434;User ID=RA046051;Password=046051;database=BD046051;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False"));
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure o pipeline de solicitação HTTP
 if (app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage();
-    // Habilitar middleware para servir o Swagger gerado como um endpoint JSON
     app.UseSwagger();
-    // Habilitar middleware para servir o swagger-ui (HTML, JS, CSS, etc.),
-    // especificando o endpoint Swagger JSON
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "AppMobile V1");
-    });
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-app.UseRouting();
+app.UseCors("AllowAllOrigins");
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
